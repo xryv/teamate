dc88fb65ff5aa18e3ecfb729ae249429458fc090
yu-Celik <celik.yus@outlook.com>
2024-02-13 20:15:17 +0100

    Mise à jour du formulaire d'inscription et du style CSS

--- src/layouts/Register.tsx
+++ src/layouts/Register.tsx
@@ -1,71 +1,128 @@
 import React, { useState } from 'react';
-import './main.css';
-import { TextField, Button } from '@mui/material';
+import { TextField, Button, Box, createTheme, ThemeProvider } from '@mui/material';
+import tw from 'twin.macro';
+import styled, { css } from 'styled-components';
 
-function RegisterPage() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: 'rgba(11, 49, 86, 0.8)', // Couleur verte
+        },
+        secondary: {
+            main: 'rgba(243, 129, 99, 1)', // Couleur existante
+        },
+    },
+});
 
-    const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        setUsername(event.target.value);
-    };
+const gradient = css`
+  background: linear-gradient(90deg, #0a3155, #172e60, #2e2966, #471d67, #5f0061, #0a3155);
+  background-size: 200% auto;
+  color: transparent;
+  -webkit-background-clip: text;
+  background-clip: text;
+  animation: gradient 5s linear infinite;
 
-    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        setPassword(event.target.value);
-    };
+  @keyframes gradient {
+    0% {background-position: 0% 50%;}
+    50% {background-position: 100% 50%;}
+    100% {background-position: 0% 50%;}
+  }
+`;
 
-    const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        setEmail(event.target.value);
-    };
+const TextGradient = styled.h1`
+  ${tw`w-full text-5xl text-center p-2 font-bold`}
+  ${gradient}
+`;
 
-    const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        setConfirmPassword(event.target.value);
-    };
-
-    const handleSubmit = (event: React.FormEvent): void => {
+function RegisterPage(): JSX.Element {
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [confirmPassword, setConfirmPassword] = useState<string>('');
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
         event.preventDefault();
-        // Ajoutez ici la logique pour envoyer les données d'inscription au serveur
+        if (password === confirmPassword &&
+            password.length > 0 &&
+            username.length > 0 &&
+            email.length > 0) {
+            console.log('Inscription réussie');
+        } else {
+            console.log('Inscription échouée');
+        }
     };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        const { name, value } = event.target;
+        switch (name) {
+            case 'username':
+                setUsername(value);
+                break;
+            case 'password':
+                setPassword(value);
+                break;
+            case 'email':
+                setEmail(value);
+                break;
+            case 'confirmPassword':
+                setConfirmPassword(value);
+                break;
+            default:
+                break;
+        }
+    };
+
+    const formFields = [
+        { label: 'Adresse e-mail', type: 'email', value: email, name: 'email', onChange: handleChange },
+        { label: "Nom d'utilisateur", type: 'text', value: username, name: 'username', onChange: handleChange },
+        { label: 'Mot de passe', type: 'password', value: password, name: 'password', onChange: handleChange },
+        { label: 'Confirmez le mot de passe', type: 'password', value: confirmPassword, name: 'confirmPassword', onChange: handleChange },
+    ];
 
     return (
-        <section className='flex flex-col w-fit h-full my-10 p-10 bg-transparant-600 rounded-xl backdrop-blur-2xl'>
+        <section className='flex flex-col items-center max-w-[70%] h-full my-10 p-10 bg-transparant-600 rounded-xl backdrop-blur-2xl'>
 
-            <h1 className='w-full text-5xl text-gradient text-center p-2 mb-10 border-b border-orangePV-900 font-bold font'>Inscription</h1>
+            <TextGradient className='w-full text-5xl text-gradient text-center p-2 font-bold font'>Inscription</TextGradient>
+            <ThemeProvider theme={theme}>
 
-            <form onSubmit={handleSubmit}>
-                <TextField
-                    label="Adresse e-mail"
-                    type="email"
-                    required
-                    value={email}
-                    onChange={handleEmailChange}
-                />
-                <TextField
-                    label="Nom d'utilisateur"
-                    required
-                    value={username}
-                    onChange={handleUsernameChange}
-                />
-                <TextField
-                    label="Mot de passe"
-                    type="password"
-                    required
-                    value={password}
-                    onChange={handlePasswordChange}
-                />
-                <TextField
-                    label="Confirmez le mot de passe"
-                    type="password"
-                    required
-                    value={confirmPassword}
-                    onChange={handleConfirmPasswordChange}
-                />
-                <Button type="submit" variant="contained" color="primary">
-      S'inscrire
-                </Button>
-            </form>
+                <hr className='w-full my-5 border-orangePV-1000' />
+
+                <Box
+                    component="form"
+                    sx={{
+                        display: 'flex', // Utilisez flex pour permettre l'alignement et la justification
+                        flexDirection: 'column', // Empile les éléments verticalement
+                        justifyContent: 'center', // Centre les éléments verticalement
+                        alignItems: 'center', // Centre les éléments horizontalement
+                        '& .MuiTextField-root': { my: 1, width: '100%' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                    onSubmit={handleSubmit}
+                >
+                    {formFields.map((field, index) => (
+                        <TextField
+                            key={index}
+                            label={field.label}
+                            type={field.type}
+                            variant="filled"
+                            color='secondary'
+                            required
+                            value={field.value}
+                            name={field.name}
+                            onChange={field.onChange}
+                        />
+                    ))}
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        disableElevation
+                        sx={{ marginTop: '20px' }}
+                    >
+                    S&apos;inscrire
+                    </Button>
+                </Box>
+            </ThemeProvider>
         </section>
     );
 }
