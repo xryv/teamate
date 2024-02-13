c24438e05c866d437c6279b49711f03455254795
yu-Celik <celik.yus@outlook.com>
2024-02-13 18:20:24 +0100

    Mise à jour des dépendances et du formulaire d'inscription

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 if (rootElement !== null) {
-    ReactDOM.createRoot(rootElement).render(
+    const root = createRoot(rootElement);
+    root.render(
         <React.StrictMode>
             <BrowserRouter>
                 <App />
